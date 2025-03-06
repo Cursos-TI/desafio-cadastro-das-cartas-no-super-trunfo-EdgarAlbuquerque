c2f2eb5d1c44b70e091987132b1b05944192bc4c
EdgarAlbuquerque <Cyborgbrasil2016@gmail.com>
2025-03-06 15:02:14 +0000

    Comparação de Populção; Comparação de Pib

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main() {                                            
+    int populacao1, populacao2; ("Curitiba e Rio de Janeiro");
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    float pib1, pib2;  ("Eua e China");
 
+    populacao1 = 1829225, ("Curitiba");
+    populacao2 = 6729894, ("Rio de Janeiro");
+
+    pib1 = 28.781, ("Eua");
+    pib2 = 18.533, ("China");
+
+    if( populacao1 > populacao2) {
+        printf("População 1 é maior que população 2\n");
+    } else{
+        printf("População 1 é menor que população 2\n");
+    }
+
+    if(pib1 > pib2) {
+        printf("Pib 1 é maior que Pib 2\n");
+    } else{
+        printf("Pib 1 é menor que Pib 2\n");
+    }
+      
     return 0;
 }
